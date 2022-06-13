ec183c0c7065e0ab26519363b82fcc531e02c701
Owen Beeks <obeeks13@gmail.com>
2022-06-13 17:44:54 +0100

    Created animator method in mover.cs
    Set NavMeshAgent to match the blend tree speed (To avoid mismatched behaviour).
    
    Created method to find velocity, make it local. Make the forwardSpeed the same as the velocity and then set it.

--- RPG Project/Assets/Player/Mover.cs
+++ RPG Project/Assets/Player/Mover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,6 +12,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -22,5 +24,12 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
