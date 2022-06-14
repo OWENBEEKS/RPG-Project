caf75631b88f86b48b7f19ace0cf0eae03bb76b4
Owen Beeks <obeeks13@gmail.com>
2022-06-14 15:39:07 +0100

    Script changes and additions
    Added Fighter, CombatTarget cs in a RPG.Combat namepsace.
    
    Using RPG.Combat in PlayerController, I check the raycast hit an object with the Fighter component and if so attack

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using RPG.Movement;
+using RPG.Combat;
 using UnityEngine;
 
 namespace RPG.Control
@@ -9,20 +11,47 @@ namespace RPG.Control
     {
         private void Update()
         {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if(target == null) continue;
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
+        {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
         }
+
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            GetMouseRay();
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
-            } 
+            }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
