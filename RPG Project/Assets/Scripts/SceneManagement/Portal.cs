01bf44f760902d130a7f0c3bea82f196f180e260
Owen Beeks <obeeks13@gmail.com>
2022-06-24 15:50:01 +0100

    Added an enum for identifying portal
    Using an enum to help identifty the portal, we can correctly send the player back to the appropriate destination

--- RPG Project/Assets/Scripts/SceneManagement/Portal.cs
+++ RPG Project/Assets/Scripts/SceneManagement/Portal.cs
@@ -1,12 +1,20 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A, B, C, D, E
+        }
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
         private void OnTriggerEnter(Collider other)
         {
              if(other.tag == "Player")
@@ -17,10 +25,36 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if(sceneToLoad < 0)
+            {
+                Debug.LogError("Scene not set to load");
+                yield break;
+            }
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);
-            print("Scene loaded");
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(gameObject);
             Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(GameObject gameObject)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(GetOtherPortal().spawnPoint.position);
+            player.transform.rotation = GetOtherPortal().spawnPoint.rotation;
+
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach(Portal portal in FindObjectsOfType<Portal>())
+            {
+                if(portal == this) continue;
+                if(portal.destination != destination) continue;
+                
+                return portal;
+            }
+            return null;
         }
     }
 }
