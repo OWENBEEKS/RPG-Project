00157ad9f6caafbb4c53f75627abdf5bffa6f23c
Owen Beeks <obeeks13@gmail.com>
2022-06-26 12:57:24 +0100

    Loads the saved scene
    Loads the saved scene, uses IEnum start to apply the faders and load.

--- RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
+++ RPG Project/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -8,7 +8,15 @@ namespace RPG.SceneManagement
 {
     public class SavingWrapper : MonoBehaviour
     {
+        [SerializeField] float fadeInTime = 0.2f;
         const string defaultSaveFile = "save";
+        IEnumerator Start()
+        {
+            Fader fader = FindObjectOfType<Fader>();
+            fader.FadeOutImmediate();
+            yield return GetComponent<SavingSystem>().LoadLastScene(defaultSaveFile);
+            yield return fader.FadeIn(fadeInTime);
+        }
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.L))
