e044deff6d5f2c8689d847b4809eccf87dfea86a
Owen Beeks <obeeks13@gmail.com>
2022-07-06 15:05:55 +0100

    Enemy health display

---
+++ RPG Project/Assets/Scripts/Combat/EnemyHealthDisplay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using RPG.Attributes;
+using TMPro;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class EnemyHealthDisplay : MonoBehaviour
+    {
+        Fighter fighter;
+
+        private void Awake()
+        {
+            fighter = GameObject.FindWithTag("Player").GetComponent<Fighter>();
+        }
+
+        private void Update()
+        {
+            if(fighter.GetTarget() == null)
+            {
+                GetComponent<TextMeshProUGUI>().text = "N/A";
+                return;
+            }
+            Health health = fighter.GetTarget();
+            GetComponent<TextMeshProUGUI>().text = $"{health.GetPercentage():0}%";
+        }
+    }
+    
+}
