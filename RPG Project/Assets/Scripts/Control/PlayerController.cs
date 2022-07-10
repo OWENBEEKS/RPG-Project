98f408641300ce0bd07dfc1e1adffe10d786d300
Owen Beeks <obeeks13@gmail.com>
2022-07-10 16:23:39 +0100

    Cursor Mapping
    Using serialize fields, struct and enum

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -11,6 +11,23 @@ namespace RPG.Control
     public class PlayerController : MonoBehaviour
     {
         Health health;
+
+        enum CursorType
+        {
+            None,
+            Movement,
+            Combat
+        }
+        [System.Serializable]
+        struct CursorMapping
+        {
+            public CursorType type; 
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] [NonReorderable] CursorMapping[] cursorMappings = null;
+
         private void Awake()
         {
             health = GetComponent<Health>();
@@ -19,7 +36,7 @@ namespace RPG.Control
         {
             if(InteractWithCombat()) return;
             if(InteractWithMovement()) return;
-            print("Nothing to do.");
+            SetCursor(CursorType.None);
         }
 
         private bool InteractWithCombat()
@@ -38,11 +55,11 @@ namespace RPG.Control
                 {
                     GetComponent<Fighter>().Attack(target.gameObject);
                 }
+                SetCursor(CursorType.Combat);
                 return true;
             }
             return false;
         }
-
         private bool InteractWithMovement()
         {
             GetMouseRay();
@@ -54,11 +71,29 @@ namespace RPG.Control
                 {
                     GetComponent<Mover>().StartMoveAction(hit.point, 1f);
                 }
+                SetCursor(CursorType.Movement);
                 return true;
             }
             return false;
         }
 
+        private void SetCursor(CursorType type)
+        {
+            CursorMapping mapping = GetCursorMapping(type);
+            Cursor.SetCursor(mapping.texture, mapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType type)
+        {
+            foreach (CursorMapping mapping in cursorMappings)
+            {
+                if(mapping.type == type)
+                {
+                    return mapping;
+                }
+            }
+            return cursorMappings[0];
+        }
         private static Ray GetMouseRay()
         {
             return Camera.main.ScreenPointToRay(Input.mousePosition);
