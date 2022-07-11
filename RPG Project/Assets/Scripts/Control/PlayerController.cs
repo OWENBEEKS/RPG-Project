5d7a0effacb852266925ca43127df04c4e20b0d6
Owen Beeks <obeeks13@gmail.com>
2022-07-11 20:42:38 +0100

    Raycast Sorting

--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -68,6 +68,19 @@ namespace RPG.Control
             return false;
         }
 
+        RaycastHit[] RaycastAllSorted()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            float[] distances = new float[hits.Length];
+            for (int i = 0; i < hits.Length; i++)
+            {
+                distances[i] = hits[i].distance;
+            }
+            Array.Sort(distances, hits);
+            return hits;
+        }
+        
+
         private bool InteractWithMovement()
         {
             RaycastHit hit;
