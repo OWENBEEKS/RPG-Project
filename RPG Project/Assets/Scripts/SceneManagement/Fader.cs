4ac3501d2999c08b2acf5b5dc234063aea752b44
Owen Beeks <obeeks13@gmail.com>
2022-07-18 20:25:36 +0100

    Fixing the fader
    Use coroutines and cancelled them where need be. Merged two routine. mathf.movetowards

--- RPG Project/Assets/Scripts/SceneManagement/Fader.cs
+++ RPG Project/Assets/Scripts/SceneManagement/Fader.cs
@@ -6,6 +6,7 @@ namespace RPG.SceneManagement
     public class Fader : MonoBehaviour
     {
         CanvasGroup canvasGroup;
+        Coroutine currentlyActiveFade = null;
         private void Awake()
         {
             canvasGroup = GetComponent<CanvasGroup>();
@@ -24,17 +25,26 @@ namespace RPG.SceneManagement
         }
         public IEnumerator FadeOut(float time)
         {
-            while(canvasGroup.alpha < 1)
-            {
-                canvasGroup.alpha += Time.deltaTime / time;
-                yield return null;
-            }
+            return Fade(1, time);
         }
         public IEnumerator FadeIn(float time)
         {
-            while(canvasGroup.alpha > 0)
+            return Fade(0, time);
+        }
+        public IEnumerator Fade(float target, float time)
+        {
+            if(currentlyActiveFade != null)
             {
-                canvasGroup.alpha -= Time.deltaTime / time;
+                StopCoroutine(currentlyActiveFade);
+            }
+            currentlyActiveFade = StartCoroutine(FadeRoutine(1, time));
+            yield return currentlyActiveFade;
+        }
+        private IEnumerator FadeRoutine(float target, float time)
+        {
+            while(Mathf.Approximately(canvasGroup.alpha, target))
+            {
+                canvasGroup.alpha = Mathf.MoveTowards(canvasGroup.alpha, target, Time.deltaTime / time);
                 yield return null;
             }
         }
